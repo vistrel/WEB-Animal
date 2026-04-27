63b18105fb2bdc30e1f2bc5edc7098d0ca1bca7f
ohotn <ohotnikegor@gmail.com>
2026-04-28 01:40:51 +0300

    add scroll to top on route change

--- client/src/layouts/MainLayout.jsx
+++ client/src/layouts/MainLayout.jsx
@@ -2,10 +2,12 @@ import { Outlet } from "react-router-dom";
 import Header from "../components/layout/Header";
 import Footer from "../components/layout/Footer";
 import PawCursor from "../components/effects/PawCursor";
+import ScrollToTop from "../components/common/ScrollToTop";
 
 function MainLayout() {
   return (
     <>
+      <ScrollToTop />
       <PawCursor />
       <Header />
 
